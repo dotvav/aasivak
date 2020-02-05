0ad4317114718c2fdcabd3b6ad76cfdfd8db0b52
dotvav <vincent.roukine@gmail.com>
2020-02-05 15:20:37 +0100

    Make the login connection timeout longer

--- Aasivak.py
+++ Aasivak.py
@@ -319,7 +319,7 @@ class HikumoAdapter:
         data = {'userId': self.config.api_username, 'userPassword': self.config.api_password}
         headers = {'user-agent': self.config.api_user_agent,
                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-        self.session.post(url, data=data, headers=headers, timeout=(2, 5))
+        self.session.post(url, data=data, headers=headers, timeout=(5, 10))
         logging.info("Logged into Hi-Kumo")
 
     def fetch_api_setup_data(self):
